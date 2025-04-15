af6c30c823d9e6fd8419e2cb762143b97a71da3f
Nikita Kuznetsov <nikita.kuznetsov@dif.tech>
2025-04-15 15:10:13 +0300

    added env variable

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,14 @@
 import com.github.kotlintelegrambot.bot
 import com.github.kotlintelegrambot.dispatch
+import com.github.kotlintelegrambot.dispatcher.channel
+import com.github.kotlintelegrambot.dispatcher.chatMember
 import com.github.kotlintelegrambot.dispatcher.text
 import com.github.kotlintelegrambot.entities.ChatId
 
 fun main(args: Array<String>) {
     val bot = bot {
-        token = "7504379155:AAEK76HT0xRH2eKyJC4wU4Lz8hMcrU3lCxE"
+        token = System.getenv("TELEGRAM_TOKEN")
+            ?: error("TELEGRAM_TOKEN environment variable is not set!")
         dispatch {
             text {
                 if (text.contains("www.instagram.com")) {
